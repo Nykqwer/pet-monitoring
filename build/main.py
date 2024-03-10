a2939396321f8f15f715e9e7bc5a63a7e55481ae
Nykqwer <nikolopezjr25@gmail.com>
2024-03-10 12:12:20 +0800

    first commit

---
+++ build/main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from login.gui7 import loginWindow
+from main_window.main import mainWindow
+
+# Main window constructor
+root = tk.Tk()  # Make temporary window for app to start
+root.withdraw()  # WithDraw the window
+
+
+if __name__ == "__main__":
+
+    loginWindow()
+    # mainWindow()
+
+    root.mainloop()
